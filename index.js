7789b4ba2bf792602a94f8e303ee92925fae73ba
William Vuong <wnvuong@vt.edu>
2015-04-26 19:41:01 -0400

    Created route to create db

--- index.js
+++ index.js
@@ -9,15 +9,10 @@ server.route({
     path: '/',
     handler: function (request, reply) {
 
-        reply('Hello, world!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+        reply({ 
+        		status: 'success',
+        		message: 'Hello, world!'
+        	});
     }
 });
 
@@ -29,9 +24,37 @@ server.route({
 		    client.query('SELECT * FROM test_table', function(err, result) {
 		      	done();
 		      	if (err)
-		       		{ console.error(err); response.send("Error " + err); }
-		      	else
-		       		{ response.send(result.rows); }
+		       	{ 
+		       		console.error(err); 
+		       		reply("Error " + err); 
+		       	} else { 
+		       		reply(result.rows); 
+		       	}
+		    });
+		});
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/createDatabase',
+	handler: function(request, reply) {
+		Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		    client.query('create table test_table', function(err, result) {
+		      	done();
+
+		      	if (err) {
+		      		reply({ 
+	        			status: 'failure',
+	        			message: err
+	        		});
+		      	} else {
+		      		reply({ 
+	        			status: 'success',
+	        			message: result
+	        		});
+		      	}
+		      	
 		    });
 		});
 	}
