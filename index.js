d44f339dec43d35fcef761029d836214bb034e99
William Vuong <wnvuong@vt.edu>
2015-04-26 19:14:15 -0400

    Commented out db code

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var Hapi = require('hapi');
 var Pg = require('pg');
 
 
-var url = 'postgres://skjgcbructmcsn:QP75bgub3gRQighsb_DJjpRlS5@ec2-54-163-238-169.compute-1.amazonaws.com:5432/d883nmfvm6vc3m';
+// var url = 'postgres://skjgcbructmcsn:QP75bgub3gRQighsb_DJjpRlS5@ec2-54-163-238-169.compute-1.amazonaws.com:5432/d883nmfvm6vc3m';
 
-var databaseURL = url;
+// var databaseURL = url;
 
-Pg.connect(databaseURL, function(err, client) {
+// Pg.connect(databaseURL, function(err, client) {
 	
-	console.log(err)
-	  var query = client.query('SELECT * FROM your_table');
+// 	console.log(err)
+// 	  var query = client.query('SELECT * FROM your_table');
 
-	  query.on('row', function(row) {
-	    console.log(JSON.stringify(row));
-	  });
-});
+// 	  query.on('row', function(row) {
+// 	    console.log(JSON.stringify(row));
+// 	  });
+// });
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT });
