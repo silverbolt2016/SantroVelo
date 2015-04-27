ee0acd3cbb682b89d7b67c20e3bd2c66d1396518
William Vuong <wnvuong@vt.edu>
2015-04-26 23:11:12 -0400

    Adding a new user route functiona

--- index.js
+++ index.js
@@ -9,19 +9,70 @@ server.route({
     path: '/',
     handler: function (request, reply) {
 
-        reply({ 
-        		status: 'success',
-        		message: 'Hello, world!'
-        	});
+      reply({ 
+      		status: 'success',
+      		message: 'Hello, world!'
+      	});
     }
 });
 
 server.route({
   method: 'POST',
   path: '/users',
-  handler: function(request, payload, reply) {
-    console.log(payload);
-    var query = '';
+  handler: function(request, reply) {
+    var payload = request.payload;
+    var invalid = '';
+
+    if (payload.firstname == '' || payload.firstname  == null ) {
+      invalid += 'firstname, ';
+    }
+    if (payload.lastname == '' || payload.lastname == null){
+      invalid += 'lastname, ';
+    } 
+    if (payload.datejoined == '' || payload.datejoined == null) {
+      invalid += 'datejoined, ';
+    }
+    if (payload.phone == '' || payload.phone == null) {
+      invalid += 'phone, ';
+    }
+    if (payload.valid       == '' || payload.valid      == null) {
+      invalid += 'valid'  
+    }
+    if (invalid != '') {
+      reply({
+            status: 'failure',
+            message: 'The following fields are required ' + invalid
+      });
+    } else {
+      Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  
+        var query = 'INSERT INTO santro_test (Firstname, Lastname, DateJoined, Phone, Valid) values (\'' + 
+          payload.firstname + '\',\'' +
+          payload.lastname + '\',\'' +
+          payload.datejoined + '\',\'' + 
+          payload.phone +'\',\'' +
+          payload.valid + '\');';
+
+        client.query(query, function(err, result) {
+          done();
+
+          var queryStatus;
+          var queryResult;
+          
+          if (err) {
+            queryStatus = 'failure';
+            queryResult = 'The user could not be added';
+          } else {
+            queryStatus = 'success',
+            queryResult = 'The user has been added'
+          }
+          reply({
+            status: queryStatus,
+            message: queryResult
+          });
+        });
+      });
+    }
   }
 })
 
@@ -32,17 +83,18 @@ server.route({
     Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       
       var query = 'SELECT * FROM santro_test WHERE ' + request.params.id + '=id';
-      
+      var queryResult;
+      var queryStatus;
+
       client.query(query, function(err, result) {
         done();
         if (err) {
-          reply({
-            status: 'failure',
-            message: 'Query could not be completed'
-          })
+          queryResult = 'failure',
+          queryStatus = 'Query could not be completed'
         } else {
-          var queryResult = 'User not found';
-          var queryStatus = 'failure';
+          queryResult = 'User not found';
+          queryStatus = 'failure';
+
           if (result.rows.length > 0) {
             queryResult = result.rows[0];
             queryStatus = 'success';
