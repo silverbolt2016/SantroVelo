618ec7db518e217486f5961aa2fcd7076cd00236
William Vuong <wnvuong@vt.edu>
2015-04-26 23:40:44 -0400

    Added missing comma

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Pg = require('pg');
 
 var pack = require('package'),
     swaggerOptions = {
-        basePath: 'https://santro-velo.herokuapp.com'
+        basePath: 'https://santro-velo.herokuapp.com',
         apiVersion: pack.version,
         info: {
           title: 'SantroVelo API Documentation',
