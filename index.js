9742109cb63821083cf72e0df7a8c201662fe9d6
William Vuong <wnvuong@vt.edu>
2015-04-26 22:07:18 -0400

    added route to user by id

--- index.js
+++ index.js
@@ -17,12 +17,51 @@ server.route({
 });
 
 server.route({
+  method: 'POST',
+  path: '/users',
+  handler: function(request, payload, reply) {
+    console.log(payload);
+    var query = '';
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/users/{id}',
+  handler: function(request, reply) {
+    Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      
+      var query = 'SELECT * FROM santro_test WHERE ' + request.params.id + '=id';
+      
+      client.query(query, function(err, result) {
+        done();
+        if (err) {
+          reply({
+            status: 'failure',
+            message: 'User not found'
+          })
+        } else {
+          reply({
+            status: 'success',
+            message: result.rows
+          })  
+        }
+        
+      });
+    
+    });
+  }
+});
+
+
+server.route({
 	method: 'GET',
 	path: '/users',
 	handler: function(request, reply) {
 		Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			// create table santro_test (id serial, LastName VARCHAR(255) NOT NULL, FirstName VARCHAR(255) NOT NULL, DateJoined DATE NOT NULL, Phone VARCHAR(10) NOT NULL, Valid BOOLEAN NOT NULL);
-		    client.query('SELECT * FROM santro_test', function(err, result) {
+      var query = 'SELECT * FROM santro_test';
+		    client.query(query, function(err, result) {
 		      	done();
 		      	if (err)
 		       	{ 
