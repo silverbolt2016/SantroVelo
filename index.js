bd4fa38374d609dbd17537847863ccea4708e2e8
William Vuong <wnvuong@vt.edu>
2015-04-26 23:20:36 -0400

    Added all users route to swagger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 var Pg = require('pg');
 
 var pack = require('package'),
@@ -16,14 +17,80 @@ server.connection({ port: process.env.PORT });
 server.route({
     method: 'GET',
     path: '/',
-    handler: function (request, reply) {
+    config: {
+      handler: function (request, reply) {
 
-      reply({ 
-      		status: 'success',
-      		message: 'Hello, world!'
-      	});
+        reply({ 
+        		status: 'success',
+        		message: 'Hello, world!'
+        	});
+      }
     }
 });
+
+
+server.route({
+  method: 'GET',
+  path: '/users',
+  config: {
+    handler: function(request, reply) {
+      Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        // create table santro_test (id serial, LastName VARCHAR(255) NOT NULL, FirstName VARCHAR(255) NOT NULL, DateJoined DATE NOT NULL, Phone VARCHAR(10) NOT NULL, Valid BOOLEAN NOT NULL);
+        var query = 'SELECT * FROM santro_test';
+          client.query(query, function(err, result) {
+              done();
+              if (err)
+              { 
+                reply("Error " + err); 
+              } else { 
+                reply({
+                  status: 'success',
+                  message: result.rows
+                }); 
+              }
+          });
+      });
+    },
+    description: 'Gets all the members of SantroVelo',
+    tags: ['api']
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/users/{id}',
+  handler: function(request, reply) {
+    Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      
+      var query = 'SELECT * FROM santro_test WHERE ' + request.params.id + '=id';
+      var queryResult;
+      var queryStatus;
+
+      client.query(query, function(err, result) {
+        done();
+        if (err) {
+          queryResult = 'failure',
+          queryStatus = 'Query could not be completed'
+        } else {
+          queryResult = 'User not found';
+          queryStatus = 'failure';
+
+          if (result.rows.length > 0) {
+            queryResult = result.rows[0];
+            queryStatus = 'success';
+          } 
+          reply({
+            status: queryStatus,
+            message: queryResult
+          });
+        }
+        
+      });
+    
+    });
+  }
+});
+
 
 server.route({
   method: 'POST',
@@ -84,65 +151,6 @@ server.route({
     }
   }
 })
-
-server.route({
-  method: 'GET',
-  path: '/users/{id}',
-  handler: function(request, reply) {
-    Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      
-      var query = 'SELECT * FROM santro_test WHERE ' + request.params.id + '=id';
-      var queryResult;
-      var queryStatus;
-
-      client.query(query, function(err, result) {
-        done();
-        if (err) {
-          queryResult = 'failure',
-          queryStatus = 'Query could not be completed'
-        } else {
-          queryResult = 'User not found';
-          queryStatus = 'failure';
-
-          if (result.rows.length > 0) {
-            queryResult = result.rows[0];
-            queryStatus = 'success';
-          } 
-          reply({
-            status: queryStatus,
-            message: queryResult
-          });
-        }
-        
-      });
-    
-    });
-  }
-});
-
-
-server.route({
-	method: 'GET',
-	path: '/users',
-	handler: function(request, reply) {
-		Pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			// create table santro_test (id serial, LastName VARCHAR(255) NOT NULL, FirstName VARCHAR(255) NOT NULL, DateJoined DATE NOT NULL, Phone VARCHAR(10) NOT NULL, Valid BOOLEAN NOT NULL);
-      var query = 'SELECT * FROM santro_test';
-		    client.query(query, function(err, result) {
-		      	done();
-		      	if (err)
-		       	{ 
-		       		reply("Error " + err); 
-		       	} else { 
-		       		reply({
-                status: 'success',
-                message: result.rows
-              }); 
-		       	}
-		    });
-		});
-	}
-});
 
 server.register({
         register: require('hapi-swagger'),
